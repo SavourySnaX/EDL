98cfc2ac2fe0a5f7448d4435cfe6018aca0f6ea3
Savoury SnaX <savoury.snax@googlemail.com>
2011-08-02 12:10:47 +0100

    Uncommented more 8080 instructions. Improved test harness so it acts correctly for io write operations.

--- testHarness.c
+++ testHarness.c
@@ -19,7 +19,9 @@ extern unsigned char PIN_D;
 extern unsigned char PIN_READY;
 extern unsigned char PIN_RESET;
 extern unsigned char PIN_SYNC;
+extern unsigned char PIN__WR;
 extern unsigned short PC;
+extern unsigned char A;
 extern unsigned char IR;
 
 void O1(void);
@@ -37,10 +39,20 @@ extern unsigned char	SYNC_INT_ACK;
 extern unsigned char	SYNC_HALT_ACK;
 extern unsigned char	SYNC_INT_ACK_HALTED;
 
+void DUMP_REGISTERS()
+{
+	printf("--------\n");
+	printf("A = %02X\n",A);
+	printf("PC= %04X\n",PC);
+	printf("--------\n");
+}
+
 void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 {
 	int a;
 	int readInstruction=0;
+	static unsigned char SYNC_LATCH;
+
 	for (a=0;a<cnt;a++)
 	{
 		O1();
@@ -56,7 +68,14 @@ void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 
 		if (PIN_SYNC)
 		{
-			if (PIN_D == SYNC_FETCH)
+			SYNC_LATCH=PIN_D;
+		}
+
+		// CPU INPUT expects data to be available on T2 state so we can do that work on the PIN_SYNC itself
+
+		if (PIN_SYNC)
+		{
+			if (SYNC_LATCH == SYNC_FETCH)
 			{
 				if (readInstruction)
 				{
@@ -70,7 +89,7 @@ void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 			}
 			else
 			{
-				if (PIN_D == SYNC_STACK_READ)
+				if (SYNC_LATCH == SYNC_STACK_READ)
 				{
 					printf("Reading memory - Address %04X <- 00\n",PIN_A);
 					PIN_D=0;
@@ -78,14 +97,13 @@ void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 				}
 				else
 				{
-					if (PIN_D == SYNC_STACK_WRITE)
+					if (SYNC_LATCH == SYNC_STACK_WRITE)
 					{
-						printf("Writing memory - Address %04X <- %02X\n",PIN_A,PIN_D);
 						PIN_READY=1;
 					}
 					else
 					{
-						if (PIN_D == SYNC_MEM_READ)
+						if (SYNC_LATCH == SYNC_MEM_READ)
 						{
 							printf("Reading memory - Address %04X <- 0\n",PIN_A);
 							PIN_D=0;
@@ -93,15 +111,30 @@ void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 						}
 						else
 						{
-							if (PIN_D == SYNC_MEM_WRITE)
+							if (SYNC_LATCH == SYNC_MEM_WRITE)
 							{
-								printf("Writing memory - Address %04X <- %02X\n",PIN_A,PIN_D);
 								PIN_READY=1;
 							}
 							else
 							{
-								printf("PIN_D = %02X\n",PIN_D);
-								exit(12);
+								if (SYNC_LATCH == SYNC_INPUT)
+								{
+									printf("Reading port %04X <- $55\n",PIN_A);
+									PIN_D=0x55;
+									PIN_READY=1;
+								}
+								else
+								{
+									if (SYNC_LATCH == SYNC_OUTPUT)
+									{
+										PIN_READY=1;
+									}
+									else
+									{
+										printf("PIN_D = %02X\n",PIN_D);
+										exit(12);
+									}
+								}
 							}
 						}
 					}
@@ -109,7 +142,33 @@ void EXECUTE_CYCLES(unsigned char instruction,int cnt)
 			}
 
 		}
+
+		// CPU OUTPUT expects device to have signalled readyness to capture at state T2, but capture occurs at T3 (when _WR is low)
+
+		if (PIN__WR == 0)
+		{
+			if (SYNC_LATCH == SYNC_STACK_WRITE)
+			{
+				printf("Writing memory - Address %04X <- %02X\n",PIN_A,PIN_D);
+			}
+			else
+			{
+				if (SYNC_LATCH == SYNC_MEM_WRITE)
+				{
+					printf("Writing memory - Address %04X <- %02X\n",PIN_A,PIN_D);
+				}
+				else
+				{
+					if (SYNC_LATCH == SYNC_OUTPUT)
+					{
+						printf("Writing port %04X <- %02X\n",PIN_A,PIN_D);
+					}
+				}
+			}
+		}
 	}
+
+	DUMP_REGISTERS();
 }
 
 int main(int argc,char**argv)
@@ -167,7 +226,23 @@ int main(int argc,char**argv)
 	EXECUTE_CYCLES(0x1F,4);		// RAR
 
 	EXECUTE_CYCLES(0x36,10);	// MVI M,$1
-	
+
+	EXECUTE_CYCLES(0x3A,13);	// LDA %$1%$0
+
+	EXECUTE_CYCLES(0x32,13);	// STA %$1%$0
+
+	EXECUTE_CYCLES(0x2A,16);	// LHLD %$1%$0
+
+	EXECUTE_CYCLES(0x22,16);	// SHLD %$1%$0
+
+	EXECUTE_CYCLES(0xC3,10);	// JMP %$1%$0
+
+	EXECUTE_CYCLES(0xCD,17);	// CALL %$1%$0
+
+	EXECUTE_CYCLES(0xDB,10);	// IN %$0
+
+	EXECUTE_CYCLES(0xD3,10);	// OUT %$0
+
 	EXECUTE_CYCLES(0,4);		// NOP
 
 	return 0;
