b6a34bc8160b0695441050948d2d3ae583500f52
Savoury SnaX <savoury.snax@googlemail.com>
2011-09-10 09:49:08 +0100

    Added version of spectrum emulation that uses the pin accurate core.

--- examples/ZXSpectrum/spectrum.c
+++ examples/ZXSpectrum/spectrum.c
@@ -13,12 +13,27 @@
 #include <string.h>
 #include <stdint.h>
 
-void STEP(void);
-void RESET(void);
-void INTERRUPT(uint8_t);
+uint16_t PinGetPIN_A();
+uint8_t PinGetPIN_D();
+void PinSetPIN_D(uint8_t);
+void PinSetPIN__CLK(uint8_t);
+uint8_t PinGetPIN__M1();
+uint8_t PinGetPIN__MREQ();
+uint8_t PinGetPIN__WR();
+uint8_t PinGetPIN__RD();
+uint8_t PinGetPIN__RFSH();
+uint8_t PinGetPIN__IORQ();
+void PinSetPIN__WAIT(uint8_t);
+void PinSetPIN__INT(uint8_t);
+void PinSetPIN__NMI(uint8_t);
+void PinSetPIN__RESET(uint8_t);
+void PinSetPIN__BUSRQ(uint8_t);
 
 unsigned char Rom[0x4000];
 unsigned char Ram[0xC000];
+
+#define TSTATES_PER_ROW		224
+#define	ROWS_PER_VBLANK		312
 
 int LoadRom(unsigned int address,unsigned int size,const char* fname)
 {
@@ -38,12 +53,15 @@ int InitialiseMemory()
 {
 	if (LoadRom(0x0000,0x4000,"roms/48k.rom"))
 		return 1;
-
+/*	if (LoadRom(0x0000,0x1000,"roms/test.rom"))
+		return 1;
+*/
 	return 0;
 }
 
 int masterClock=0;
 int pixelClock=0;
+int tapeClock=0;
 
 int KeyDown(int key);
 int CheckKey(int key);
@@ -140,21 +158,23 @@ uint32_t GetColour(uint8_t attr,uint8_t pixSet)
 	return colour;
 }
 
-void ClearBorderColour()
+void ClearBorderColour(int y)
 {
 	uint32_t* outputTexture = (uint32_t*)(videoMemory[MAIN_WINDOW]);
-	int x,y;
+	int x;
 
 	uint32_t BGRA;
 
+	if (y==ROWS_PER_VBLANK-1)
+		flashTimer++;
+	if (y>=HEIGHT)
+		return;
+
 	BGRA=GetColour(borderCol,1);
 
-	for (y=0;y<HEIGHT;y++)
+	for (x=0;x<WIDTH;x++)
 	{
-		for (x=0;x<WIDTH;x++)
-		{
-			outputTexture[x+y*WIDTH]=BGRA;
-		}
+		outputTexture[x+y*WIDTH]=BGRA;
 	}
 }
 
@@ -168,15 +188,19 @@ uint8_t *GetAttrPtr()
 	return GetScreenPtr()+24*32*8;
 }
 
-void VID_DrawScreen()
+void VID_DrawScreenRow(int y)
 {
 	uint32_t* outputTexture = (uint32_t*)(videoMemory[MAIN_WINDOW]);
 	uint8_t *screenPtr = GetScreenPtr();
 	uint8_t *attrPtr = GetAttrPtr();
-	int x,sx,y;
+	int x,sx;
 
-	ClearBorderColour();
-	for (y=0;y<HEIGHT-2*BORDERWIDTH;y++)
+	ClearBorderColour(y);
+
+	if (y<BORDERWIDTH || y>=HEIGHT-BORDERWIDTH)
+		return;
+
+	y-=BORDERWIDTH;
 	{
 		uint8_t *rowPtr = &screenPtr[(y/64)*(8*8*32) + (y&7)*32*8 + ((y&63)>>3)*32];
 
@@ -197,13 +221,10 @@ void VID_DrawScreen()
 			}
 		}
 	}
-	flashTimer++;
 }
 
 void ShowScreen(int windowNum,int w,int h)
 {
-	VID_DrawScreen();
-
 	glBindTexture(GL_TEXTURE_RECTANGLE_NV, videoTexture[windowNum]);
 	
 	// glTexSubImage2D is faster when not using a texture range
@@ -287,9 +308,6 @@ void kbHandler( GLFWwindow window, int key, int action )		/* At present ignores 
 extern unsigned short PC;
 extern unsigned char CYCLES;
 
-#define TSTATES_PER_ROW		224
-#define	ROWS_PER_VBLANK		312
-
 extern uint8_t *DIS_[256];
 
 uint8_t GetByte(uint16_t addr)
@@ -338,6 +356,10 @@ uint8_t CheckKeys(uint8_t scan,int key0,int key1,int key2,int key3,int key4)
 	return retVal;
 }
 
+void LoadTape();
+void UpdateTape(uint8_t cycles);
+uint8_t GetTapeLevel();
+
 uint8_t GetPort(uint16_t port)
 {
 	uint8_t matrixLine = ~(port>>8);
@@ -357,6 +379,10 @@ uint8_t GetPort(uint16_t port)
 		retVal=(~retVal)&0x1F;
 
 		retVal|=0xA0;			// apparantly always logic 1
+		if (GetTapeLevel()>=0x80)
+		{
+			retVal|=0x40;
+		}
 
 		return retVal;
 	}
@@ -422,6 +448,12 @@ const char* decodeDisasm(uint8_t *table[256],unsigned int address,int *count)
 	char* dPtr=temporaryBuffer;
 	int counting = 0;
 	int doingDecode=0;
+	
+	if (sPtr==NULL)
+	{
+		sprintf(temporaryBuffer,"UNKNOWN OPCODE");
+		return temporaryBuffer;
+	}
 
 	if (strcmp(mnemonic,"CB")==0)
 	{
@@ -465,7 +497,7 @@ const char* decodeDisasm(uint8_t *table[256],unsigned int address,int *count)
 		*count=tmpCount+1;
 		return temporaryBuffer;
 	}
-
+	
 	while (*sPtr)
 	{
 		if (!doingDecode)
@@ -505,13 +537,27 @@ const char* decodeDisasm(uint8_t *table[256],unsigned int address,int *count)
 	return temporaryBuffer;
 }
 
-void Disassemble(unsigned int address)
+int Disassemble(unsigned int address,int registers)
 {
 	int a;
 	int numBytes=0;
 	const char* retVal = decodeDisasm(DIS_,address,&numBytes);
 
-	DUMP_REGISTERS();
+	if (strcmp(retVal,"UNKNOWN OPCODE")==0)
+	{
+		printf("UNKNOWN AT : %04X\n",address);
+		for (a=0;a<numBytes+1;a++)
+		{
+			printf("%02X ",GetByte(address+a));
+		}
+		printf("\n");
+		exit(-1);
+	}
+
+	if (registers)
+	{
+		DUMP_REGISTERS();
+	}
 	printf("%04X :",address);
 
 	for (a=0;a<numBytes+1;a++)
@@ -523,12 +569,41 @@ void Disassemble(unsigned int address)
 		printf("   ");
 	}
 	printf("%s\n",retVal);
+
+	return numBytes+1;
 }
+
+void SHOW_PINS()
+{
+	printf("%s %s %s %s %s %s %04X %02X\n",
+			PinGetPIN__M1()?"  ":"M1",
+			PinGetPIN__MREQ()?"    ":"MREQ",
+			PinGetPIN__RD()?"  ":"RD",
+			PinGetPIN__WR()?"  ":"WR",
+			PinGetPIN__RFSH()?"    ":"RFSH",
+			PinGetPIN__IORQ()?"    ":"IORQ",
+			PinGetPIN_A(),
+			PinGetPIN_D());
+}
+
+#define SCREEN_ON	1
+
+void DisassembleRange(unsigned int start,unsigned int end)
+{
+	while (start<end)
+	{
+		start+=Disassemble(start,0);
+	}
+}	
 
 int main(int argc,char**argv)
 {
 	double	atStart,now,remain;
+	int a;
+	int intClocks=0;
+	int curRow=0;
 
+#if SCREEN_ON
 	/// Initialize GLFW 
 	glfwInit(); 
 
@@ -549,36 +624,110 @@ int main(int argc,char**argv)
 	glfwSetKeyCallback(kbHandler);
 
 	atStart=glfwGetTime();
+#endif
 	//////////////////
 
+	LoadTape();
 	if (InitialiseMemory())
 		return -1;
 	
-	RESET();
+	PinSetPIN__WAIT(1);
+	PinSetPIN__INT(1);
+	PinSetPIN__NMI(1);
+	PinSetPIN__RESET(1);
+	PinSetPIN__BUSRQ(1);
 
-	while (!glfwGetKey(windows[MAIN_WINDOW],GLFW_KEY_ESC))
+	PinSetPIN__RESET(0);
+	PinSetPIN__CLK(1);
+	PinSetPIN__CLK(0);
+	PinSetPIN__CLK(1);
+	PinSetPIN__CLK(0);
+	PinSetPIN__CLK(1);
+	PinSetPIN__CLK(0);
+	PinSetPIN__RESET(1);
+	
+//	DisassembleRange(0x0000,0x4000);
+
+	while (1==1)
 	{
-//		Disassemble(PC);
-		STEP();
+		static int doDebug=0;
+		PinSetPIN__INT(1);
+		if (intClocks!=0)
+		{
+			PinSetPIN__INT(0);
+			intClocks--;
+		}
 
-		masterClock+=CYCLES;
-		while (masterClock>=TSTATES_PER_ROW)
+		PinSetPIN__CLK(1);
+
+		if ((!PinGetPIN__M1()) && PinGetPIN__MREQ())		// NOT PERFECT, SINCE IT WILL CATCH INTERRUPT REQUESTS.. 
+		{
+			// First cycle of a new instruction
+			if (doDebug)
+				Disassemble(PinGetPIN_A(),1);
+		}
+		if ((!PinGetPIN__M1()) && PinGetPIN__IORQ())
+		{
+			PinSetPIN_D(0xFF);
+		}
+		if ((!PinGetPIN__MREQ()) && (!PinGetPIN__RD()))
+		{
+			uint16_t addr = PinGetPIN_A();
+			uint8_t  data = GetByte(addr);
+			PinSetPIN_D(data);
+		}
+		if ((!PinGetPIN__IORQ()) && (!PinGetPIN__RD()))
+		{
+			uint16_t addr = PinGetPIN_A();
+			uint8_t  data = GetPort(addr);
+			PinSetPIN_D(data);
+		}
+		if ((!PinGetPIN__MREQ()) && (!PinGetPIN__WR()))
+		{
+			SetByte(PinGetPIN_A(),PinGetPIN_D());
+		}
+		if ((!PinGetPIN__IORQ()) && (!PinGetPIN__WR()))
+		{
+			SetPort(PinGetPIN_A(),PinGetPIN_D());
+		}
+	
+		PinSetPIN__CLK(0);
+
+		masterClock+=1;
+
+		UpdateTape(1);
+		
+		if (masterClock>=TSTATES_PER_ROW)
 		{	
 			pixelClock+=1;
+			VID_DrawScreenRow(curRow);
+			curRow++;
+
 			masterClock-=TSTATES_PER_ROW;
 		}
 		if (pixelClock>=ROWS_PER_VBLANK)
-		{
+		{	
+			curRow=0;
 			pixelClock-=ROWS_PER_VBLANK;
 
-			INTERRUPT(0xFF);
+			intClocks=32;
 
             		glfwMakeContextCurrent(windows[MAIN_WINDOW]);
 			ShowScreen(MAIN_WINDOW,WIDTH,HEIGHT);
 			glfwSwapBuffers();
 				
 			glfwPollEvents();
-			
+		
+			if (glfwGetKey(windows[MAIN_WINDOW],GLFW_KEY_ESC))
+			{
+				break;
+			}
+			if (glfwGetKey(windows[MAIN_WINDOW],' '))
+			{
+				doDebug=1;
+			}
+
+
 			now=glfwGetTime();
 
 			remain = now-atStart;
@@ -592,8 +741,76 @@ int main(int argc,char**argv)
 			atStart=glfwGetTime();
 		}
 	}
-	
 	return 0;
 
+}
+
+// Tape handling
+uint32_t			tapePos=0;
+uint32_t			tapeCycles=0;
+uint8_t				casLevel=0;
+uint8_t*			RawTapFile=NULL;
+uint32_t			RawTapLength=0;
+
+unsigned char *qLoad(char *romName,uint32_t *length)
+{
+	FILE *inRom;
+	unsigned char *romData;
+	*length=0;
+
+	inRom = fopen(romName,"rb");
+	if (!inRom)
+	{
+		return 0;
+	}
+	fseek(inRom,0,SEEK_END);
+	*length = ftell(inRom);
+
+	fseek(inRom,0,SEEK_SET);
+	romData = (unsigned char *)malloc(*length);
+	if (romData)
+	{
+		if ( *length != fread(romData,1,*length,inRom))
+		{
+			free(romData);
+			*length=0;
+			romData=0;
+		}
+	}
+	fclose(inRom);
+
+	return romData;
+}
+
+void LoadTape()
+{
+	RawTapFile=qLoad("jsw.raw",&RawTapLength);
+	if (!RawTapFile)
+	{
+		printf("Failed to load file\n");
+		RawTapFile=malloc(1);
+		RawTapLength=1;
+	}
+	tapePos=0;
+	casLevel=RawTapFile[tapePos];
+}
+
+void UpdateTape(uint8_t cycles)
+{
+	tapeClock+=cycles*4096;
+	if (tapeClock>=(TSTATES_PER_ROW*ROWS_PER_VBLANK*4096)/((44100)/50))
+	{
+		tapeClock-=(TSTATES_PER_ROW*ROWS_PER_VBLANK*4096)/((44100)/50);
+		tapePos++;
+		if (tapePos>RawTapLength)
+			tapePos=0;
+
+		casLevel=RawTapFile[tapePos];
+	}
+}
+
+uint8_t GetTapeLevel()
+{
+	return casLevel;
 }
 
