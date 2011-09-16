7843c6a71e58fbd1262f15256915fa7afff6d0bf
Savoury SnaX <savoury.snax@googlemail.com>
2011-09-16 12:15:13 +0100

    tzx loading fixes

--- examples/ZXSpectrum/common.c
+++ examples/ZXSpectrum/common.c
@@ -750,17 +750,21 @@ uint32_t	tapeFormat=0;
 
 uint32_t	tapeDataLength;
 uint8_t		tapeDataBit;
+uint8_t		tapeDataBitCnt;
 uint16_t	tapePilotLength;
 uint32_t	tapePilotPulseLength;
 
 uint16_t	tapeParamPilotLength;
+uint16_t	tapeParamLoopCounter;
+uint32_t	tapeParamLoopMarker;
 uint32_t	tapeParamPilotPulseLength;
 uint32_t	tapeParamSync1Length;
 uint32_t	tapeParamSync2Length;
 uint32_t	tapeParamOneLength;
 uint32_t	tapeParamZeroLength;
 uint32_t	tapeParamDelayLength;
-
+uint8_t		tapeParamUsedBits;
+uint32_t	tapeParamLastCase;
 
 unsigned char *qLoad(const char *romName,uint32_t *length)
 {
@@ -893,6 +897,14 @@ int CheckTZX()
 				blockLength|=tapeFile[curPos++]<<16;
 				curPos+=blockLength;
 				break;
+			case 0x12:
+				printf("Pure Tone\n");
+				curPos+=4;
+				break;
+			case 0x13:
+				printf("Pulse Sequence\n");
+				curPos+=tapeFile[curPos]*2+1;
+				break;
 			case 0x14:
 				printf("Pure Data\n");
 				curPos+=7;
@@ -901,12 +913,23 @@ int CheckTZX()
 				blockLength|=tapeFile[curPos++]<<16;
 				curPos+=blockLength;
 				break;
+			case 0x20:
+				printf("Silence/Stop Tape\n");
+				curPos+=2;
+				break;
 			case 0x21:
 				printf("Group Start\n");
 				curPos+=tapeFile[curPos]+1;
 				break;
 			case 0x22:
 				printf("Group End\n");
+				break;
+			case 0x24:
+				printf("Loop Start\n");
+				curPos+=2;
+				break;
+			case 0x25:
+				printf("Loop End\n");
 				break;
 			case 0x30:
 				printf("Text Description\n");
@@ -940,6 +963,8 @@ void LoadTapeFormat(const char* filename)		// Currently handles (raw audio, .TAP
 	if (!tapeFile)
 	{
 		printf("Failed to load file - %s (ignoring)\n");
+		tapeFormat=99;
+		return;
 	}
 
 	tapePos=0;
@@ -978,7 +1003,6 @@ uint8_t GetNextBlockTZX()
 				printf("Standard\n");
 				tapeParamDelayLength=tapeFile[tapePos++];
 				tapeParamDelayLength|=tapeFile[tapePos++]<<8;
-				tapeParamDelayLength=tapeParamDelayLength*3494.4f;
 				tapeDataLength=tapeFile[tapePos++];
 				tapeDataLength|=tapeFile[tapePos++]<<8;
 				tapeParamPilotPulseLength=2168;
@@ -996,6 +1020,8 @@ uint8_t GetNextBlockTZX()
 				tapeParamSync2Length=735;
 				tapeParamOneLength=1710;
 				tapeParamZeroLength=855;
+				tapeParamLastCase=8;
+				tapeParamUsedBits=8;
 				return 1;
 			case 0x11:
 				printf("Speed\n");
@@ -1011,34 +1037,83 @@ uint8_t GetNextBlockTZX()
 				tapeParamOneLength|=tapeFile[tapePos++]<<8;
 				tapeParamPilotLength=tapeFile[tapePos++];
 				tapeParamPilotLength|=tapeFile[tapePos++]<<8;
-				tapePos++;	// Used bits not handled yet
+				tapeParamUsedBits=tapeFile[tapePos++];
 				tapeParamDelayLength=tapeFile[tapePos++];
 				tapeParamDelayLength|=tapeFile[tapePos++]<<8;
-				tapeParamDelayLength=tapeParamDelayLength*3494.4f;
 				tapeDataLength=tapeFile[tapePos++];
 				tapeDataLength|=tapeFile[tapePos++]<<8;
 				tapeDataLength|=tapeFile[tapePos++]<<16;
+				if (tapeDataLength==1)
+				{
+					tapeDataBitCnt=tapeParamUsedBits;
+				}
+				tapeParamLastCase=8;
 				return 1;
+			case 0x12:
+				printf("Pure Tone\n");
+				tapeParamPilotPulseLength=tapeFile[tapePos++];
+				tapeParamPilotPulseLength|=tapeFile[tapePos++]<<8;
+				tapeParamPilotLength=tapeFile[tapePos++];
+				tapeParamPilotLength|=tapeFile[tapePos++]<<8;
+				tapeParamLastCase=1;
+				return 1;
+			case 0x13:
+				printf("Pulse Sequence\n");
+				tapeParamPilotLength=tapeFile[tapePos++];
+				tapeParamPilotPulseLength=tapeFile[tapePos++];
+				tapeParamPilotPulseLength|=tapeFile[tapePos++]<<8;
+				tapeParamLastCase=9;
+				return 9;
 			case 0x14:
 				printf("Pure Data\n");
 				tapeParamZeroLength=tapeFile[tapePos++];
 				tapeParamZeroLength|=tapeFile[tapePos++]<<8;
 				tapeParamOneLength=tapeFile[tapePos++];
 				tapeParamOneLength|=tapeFile[tapePos++]<<8;
-				tapePos++;	// Used bits not handled yet
+				tapeParamUsedBits=tapeFile[tapePos++];
 				tapeParamDelayLength=tapeFile[tapePos++];
 				tapeParamDelayLength|=tapeFile[tapePos++]<<8;
-				tapeParamDelayLength=tapeParamDelayLength*3494.4f;
 				tapeDataLength=tapeFile[tapePos++];
 				tapeDataLength|=tapeFile[tapePos++]<<8;
 				tapeDataLength|=tapeFile[tapePos++]<<16;
+				if (tapeDataLength==1)
+				{
+					tapeDataBitCnt=tapeParamUsedBits;
+				}
+				tapeParamLastCase=8;
 				return 4;
+			case 0x20:
+				printf("Silence/Stop The Tape\n");
+				tapeParamDelayLength=tapeFile[tapePos++];
+				tapeParamDelayLength|=tapeFile[tapePos++]<<8;
+				if (tapeParamDelayLength==0)
+				{
+					tapeLevel=0;
+					tapePaused=1;
+					return 0;
+				}
+				tapeParamLastCase=8;
+				return 7;
 			case 0x21:
 				printf("Group Start\n");
 				tapePos+=tapeFile[tapePos]+1;
 				break;
 			case 0x22:
 				printf("Group End\n");
+				break;
+			case 0x24:
+				printf("Loop Start\n");
+				tapeParamLoopCounter=tapeFile[tapePos++];
+				tapeParamLoopCounter|=tapeFile[tapePos++]<<8;
+				tapeParamLoopMarker=tapePos;
+				break;
+			case 0x25:
+				printf("Loop End\n");
+				tapeParamLoopCounter--;
+				if (tapeParamLoopCounter!=0)
+				{
+					tapePos=tapeParamLoopMarker;
+				}
 				break;
 			case 0x30:
 				printf("Text Description\n");
@@ -1087,6 +1162,8 @@ int GetNextBlockParamsTape()
 		tapeParamSync2Length=735;
 		tapeParamOneLength=1710;
 		tapeParamZeroLength=855;
+		tapeParamLastCase=8;
+		tapeParamUsedBits=8;
 		return 1;
 	}
 	if (tapeFormat==2)
@@ -1100,7 +1177,7 @@ void UpdateTape(uint8_t cycles)
 	int redo=1;
 	if (!tapeFile || tapePaused || !cycles)
 		return;
-			
+	
 	while (cycles||redo)
 	{
 		cycles--;
@@ -1131,6 +1208,10 @@ void UpdateTape(uint8_t cycles)
 					{
 						tapePhase=2;
 						tapePilotPulseLength=tapeParamSync1Length;
+						if (tapeParamLastCase==1)
+						{
+							tapePhase=0;
+						}
 					}
 				}
 				break;
@@ -1151,6 +1232,11 @@ void UpdateTape(uint8_t cycles)
 					tapeTCounter-=tapePilotPulseLength;
 					tapeLevel=~tapeLevel;
 					tapeDataBit=0x80;
+					tapeDataBitCnt=8;
+					if (tapeDataLength==1)
+					{
+						tapeDataBitCnt=tapeParamUsedBits;
+					}
 					tapePhase=4;
 				}
 				break;
@@ -1158,7 +1244,6 @@ void UpdateTape(uint8_t cycles)
 				// For each byte in tapDataLength
 				// We get a bit, set the correct pulse length
 				// rinse and repeat
-
 				if (tapeFile[tapePos]&tapeDataBit)
 				{
 					tapePilotPulseLength=tapeParamOneLength;
@@ -1185,22 +1270,31 @@ void UpdateTape(uint8_t cycles)
 					tapeTCounter-=tapePilotPulseLength;
 					tapeLevel=~tapeLevel;
 					tapeDataBit>>=1;
+					tapeDataBitCnt--;
 					tapePhase=4;
-					if (tapeDataBit==0)
+					if (tapeDataBitCnt==0)
 					{
 						tapeDataBit=0x80;
-						tapePos++;
+						tapeDataBitCnt=8;
 						tapeDataLength--;
-						if (tapeDataLength==0)
+						tapePos++;
+						if (tapeDataLength==1)
 						{
-							tapePilotPulseLength=tapeParamDelayLength;
-							if (tapePilotPulseLength==0)
+							tapeDataBitCnt=tapeParamUsedBits;
+						}
+						else
+						{
+							if (tapeDataLength==0)
 							{
-								tapePhase=0;
-							}
-							else
-							{
-								tapePhase=7;
+								if (tapeParamDelayLength==0)
+								{
+									tapePhase=0;
+								}
+								else
+								{
+									tapePilotPulseLength=1*3494.4f;
+									tapePhase=7;
+								}
 							}
 						}
 					}
@@ -1211,8 +1305,39 @@ void UpdateTape(uint8_t cycles)
 				if (tapeTCounter>=tapePilotPulseLength)
 				{
 					tapeTCounter-=tapePilotPulseLength;
+					tapePilotPulseLength=(tapeParamDelayLength-1)*3494.4f;
+					tapePhase=8;
+					if (tapePilotPulseLength==0)
+					{
+						tapePhase=0;
+					}
 					tapeLevel=0;					// After a silence block, level must be 0
+				}
+				break;
+			case 8:
+				tapeTCounter++;
+				if (tapeTCounter>=tapePilotPulseLength)
+				{
+					tapeTCounter-=tapePilotPulseLength;
 					tapePhase=0;
+				}
+				break;
+			case 9:								// Pilot pulse sequence special case
+				tapeTCounter++;
+				if (tapeTCounter>=tapeParamPilotPulseLength)
+				{
+					tapeTCounter-=tapeParamPilotPulseLength;
+					tapeLevel=~tapeLevel;
+					tapeParamPilotLength--;
+					if (tapeParamPilotLength==0)
+					{
+						tapePhase=0;
+					}
+					else
+					{
+						tapeParamPilotPulseLength=tapeFile[tapePos++];
+						tapeParamPilotPulseLength|=tapeFile[tapePos++]<<8;
+					}
 				}
 				break;
 
@@ -1227,7 +1352,7 @@ void UpdateTape(uint8_t cycles)
 						tapePos=0;
 					}
 
-					tapeLevel=tapeFile[tapePos];
+					tapeLevel=tapeFile[tapePos]<0x80?0x00:0xFF;
 				}
 				break;
 		}
