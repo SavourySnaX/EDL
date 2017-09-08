23e5bf3e6b39bc421b0378ed02477148d0aa16bd
Savoury SnaX <savoury.snax@googlemail.com>
2017-09-08 07:46:39 +0100

    Upgrade states test to perform a self test.

--- tests/states/test.c
+++ tests/states/test.c
@@ -3,12 +3,30 @@
 
 void PinSettest(uint8_t);
 
+extern uint8_t stateOrder[16];
+extern uint8_t bingoCnt;
+extern uint8_t bangoCnt;
+
 int main(int argc,char** argv)
 {
+	uint8_t orderCheck[16] = { 0,1,2,3,4,5,8,9,6,7 };
 	int a;
 	for (a=0;a<10;a++)
 	{
 		PinSettest(0);
+	}
+
+	if (bingoCnt != 5)
+		return 1;
+	if (bangoCnt != 2)
+		return 1;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (stateOrder[a] != orderCheck[a])
+		{
+			return 1;
+		}
 	}
 
 	return 0;
